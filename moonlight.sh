8a51b67d7e4fb14bf825fc0257f564d67d147ab5
lucas k <lucas.kluber@gmail.com>
2018-10-20 05:55:07 -0300

    -AAA

--- moonlight.sh
+++ moonlight.sh
@@ -2,7 +2,7 @@
 set -e # exit on error
 
 if [ -f "$home_dir"/RetroPie/roms/moonlight/moonlight.sh ]; then
-	wd='"$home_dir"/RetroPie/roms/moonlight'
+	wd="$home_dir"/RetroPie/roms/moonlight
 else
 	wd="`pwd`"
 fi
